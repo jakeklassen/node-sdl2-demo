f5700953dc815732deb3dd17528c00a0915a0e4e
Jake Klassen <jklassendev@gmail.com>
2023-04-04 09:29:21 -0400

    trying to scale image

--- src/canvas.js
+++ src/canvas.js
@@ -1,13 +1,33 @@
 import sdl from "@kmamal/sdl";
-import Canvas from "canvas";
+import Canvas, { loadImage } from "canvas";
 
-const window = sdl.video.createWindow({ title: "Canvas" });
+const ship = await loadImage("assets/player-ship.png");
+
+const gameWidth = 128;
+const gameHeight = 128;
+const scale = 4;
+
+const window = sdl.video.createWindow({
+  title: "Canvas",
+  width: gameWidth * scale,
+  height: gameHeight * scale,
+  vsync: true,
+});
+
 const { width, height } = window;
-const canvas = Canvas.createCanvas(width, height);
+const canvas = Canvas.createCanvas(gameWidth, gameHeight);
+
 const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
 
 ctx.fillStyle = "black";
-ctx.fillRect(0, 0, width, height);
+ctx.fillRect(0, 0, gameWidth, gameHeight);
+ctx.drawImage(
+  ship,
+  canvas.width / 2 - ship.width / 2,
+  canvas.height / 2 - ship.height / 2,
+);
+
 const buffer = canvas.toBuffer("raw");
 
-window.render(width, height, width * 4, "bgra32", buffer);
+window.render(gameWidth, gameHeight, gameWidth * 4, "bgra32", buffer);
