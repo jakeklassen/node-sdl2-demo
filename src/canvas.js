b34118ee105f823dfc2de58f9f5d90660073187c
Jake Klassen <jklassendev@gmail.com>
2023-04-06 14:14:22 -0400

    saving work

--- src/canvas.js
+++ src/canvas.js
@@ -1,6 +1,7 @@
 import sdl from "@kmamal/sdl";
 import Canvas, { loadImage } from "canvas";
 import { setTimeout } from "timers/promises";
+import sharp from "sharp";
 
 const ship = await loadImage("assets/player-ship.png");
 
@@ -17,8 +18,8 @@ const window = sdl.video.createWindow({
 
 const canvas = Canvas.createCanvas(gameWidth, gameHeight);
 
-const ctx = canvas.getContext("2d");
-ctx.imageSmoothingEnabled = false;
+const context = canvas.getContext("2d");
+context.imageSmoothingEnabled = false;
 
 /**
  * Draw the sprite with the flash effect
@@ -65,26 +66,238 @@ function drawFlashingSprite(
   context.restore();
 }
 
+/**
+ *
+ * @param {Canvas.CanvasRenderingContext2D} context
+ * @param {number} cx
+ * @param {number} cy
+ * @param {number} r
+ * @param {string} color
+ */
+function fillPixelatedCircle(context, cx, cy, r, color) {
+  r |= 0; // floor radius
+
+  // context.setTransform(1, 0, 0, 1, 0, 0); // ensure default transform
+
+  var x = r,
+    y = 0,
+    dx = 1,
+    dy = 1;
+
+  var err = dx - (r << 1);
+  var x0 = (cx - 1) | 0,
+    y0 = cy | 0;
+  var lx = x,
+    ly = y;
+
+  context.fillStyle = color;
+
+  context.beginPath();
+
+  while (x >= y) {
+    context.rect(x0 - x, y0 + y, x * 2 + 2, 1);
+    context.rect(x0 - x, y0 - y, x * 2 + 2, 1);
+    if (x !== lx) {
+      context.rect(x0 - ly, y0 - lx, ly * 2 + 2, 1);
+      context.rect(x0 - ly, y0 + lx, ly * 2 + 2, 1);
+    }
+    lx = x;
+    ly = y;
+    y++;
+    err += dy;
+    dy += 2;
+    if (err > 0) {
+      x--;
+      dx += 2;
+      err += (-r << 1) + dx;
+    }
+  }
+  if (x !== lx) {
+    context.rect(x0 - ly, y0 - lx, ly * 2 + 1, 1);
+    context.rect(x0 - ly, y0 + lx, ly * 2 + 1, 1);
+  }
+  context.fill();
+}
+
+/**
+ * Implementation of the PICO-8 `circfill` function
+ * @param {Canvas.CanvasRenderingContext2D} context
+ * @param {number} x
+ * @param {number} y
+ * @param {number} radius
+ * @param {string} color
+ */
+function fillCircle(context, x, y, radius, color) {
+  let x1 = 0;
+  let y1 = radius;
+  let diameter = 3 - 2 * radius;
+
+  while (x1 <= y1) {
+    // Draw horizontal lines of the circle
+    for (let i = -x1; i <= x1; i++) {
+      context.fillStyle = color;
+      context.fillRect(x + i, y + y1, 1, 1);
+      context.fillRect(x + i, y - y1, 1, 1);
+    }
+
+    // Draw vertical lines of the circle
+    for (let i = -y1; i <= y1; i++) {
+      context.fillStyle = color;
+      context.fillRect(x + i, y + x1, 1, 1);
+      context.fillRect(x + i, y - x1, 1, 1);
+    }
+
+    if (diameter < 0) {
+      diameter = diameter + 4 * x1 + 6;
+    } else {
+      diameter = diameter + 4 * (x1 - y1) + 10;
+      y1--;
+    }
+
+    x1++;
+  }
+}
+
 const flashDuration = 200;
 const flashColor = "#ffffff";
 
-while (!window.destroyed) {
-  drawFlashingSprite(
-    ctx,
-    Math.floor(gameWidth / 2 - ship.width / 2),
-    Math.floor(gameHeight / 2 - ship.height / 2),
-    ship.width,
-    ship.height,
-    flashColor,
-    flashDuration,
-    performance.now(),
-  );
+const tintCanvas = Canvas.createCanvas(gameWidth, gameHeight);
 
-  ctx.drawImage(
+const tintContext = tintCanvas.getContext("2d");
+tintContext.imageSmoothingEnabled = false;
+
+const bufferCanvas = Canvas.createCanvas(gameWidth, gameHeight);
+
+const bufferContext = bufferCanvas.getContext("2d");
+bufferContext.imageSmoothingEnabled = false;
+
+/**
+ *
+ * @param {Canvas.CanvasRenderingContext2D} context
+ * @param {number} time
+ * @param {string} flashColor
+ */
+function drawSprites(context, time, flashColor) {
+  // Clear the canvas
+  tintContext.clearRect(0, 0, tintCanvas.width, tintCanvas.height);
+  bufferContext.clearRect(0, 0, bufferCanvas.width, bufferCanvas.height);
+
+  tintContext.save();
+
+  // Loop through each sprite and draw it
+  for (const sprite of sprites) {
+    // Calculate the alpha value based on the flashing duration and the sprite's flash flag
+    const alpha = sprite.flash
+      ? (Math.sin((time / sprite.flashDuration) * Math.PI * 2) + 1) / 2
+      : 1; // oscillate between 0 and 1 if flash is true, else use 1
+
+    // Convert the flashColor from hex to RGBA
+    const red = parseInt(flashColor.substring(1, 3), 16);
+    const green = parseInt(flashColor.substring(3, 5), 16);
+    const blue = parseInt(flashColor.substring(5, 7), 16);
+    const rgba = `rgba(${red},${green},${blue},${alpha})`;
+
+    // Draw the sprite with the color tint applied if flash is true
+    if (sprite.flash) {
+      tintContext.fillStyle = rgba;
+      // tintContext.fillRect(
+      //   sprite.x,
+      //   sprite.y,
+      //   sprite.image.width,
+      //   sprite.image.height
+      // );
+
+      tintContext.beginPath();
+      tintContext.arc(
+        sprite.x + sprite.image.width / 2,
+        sprite.y + sprite.image.height / 2,
+        4,
+        0,
+        2 * Math.PI,
+      );
+      tintContext.fill();
+    }
+
+    bufferContext.drawImage(
+      sprite.image,
+      sprite.x,
+      sprite.y,
+      sprite.image.width,
+      sprite.image.height,
+    );
+  }
+
+  tintContext.globalCompositeOperation = "destination-atop";
+
+  tintContext.drawImage(bufferCanvas, 0, 0);
+
+  tintContext.restore();
+
+  context.drawImage(tintCanvas, 0, 0);
+}
+
+/**
+ * @type {import("./sprite.js").Sprite[]}
+ */
+const sprites = [
+  {
+    image: ship,
+    x: Math.floor(canvas.width / 2 - ship.width / 2),
+    y: Math.floor(canvas.height / 2 - ship.height / 2),
+    flash: true,
+    flashDuration: 200,
+  },
+  {
+    image: ship,
+    x: Math.floor(canvas.width / 2 - ship.width / 2),
+    y: Math.floor(canvas.height / 2 - ship.height / 2) + 20,
+    flash: true,
+    flashDuration: 200,
+  },
+  {
+    image: ship,
+    x: Math.floor(canvas.width / 2 - ship.width / 2) + 4,
+    y: Math.floor(canvas.height / 2 - ship.height / 2) + 24,
+    flash: true,
+    flashDuration: 400,
+  },
+];
+
+while (!window.destroyed) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawSprites(context, performance.now(), flashColor);
+
+  context.drawImage(
     ship,
     Math.floor(gameWidth / 2 - ship.width / 2),
-    Math.floor(gameHeight / 2 - ship.height / 2) + 20,
+    Math.floor(gameHeight / 2 - ship.height / 2) + 40,
   );
+
+  context.fillStyle = "#ffffff";
+  context.strokeStyle = "#ffffff";
+  context.lineWidth = 1;
+
+  context.beginPath();
+  // Ewwww have to do this to get a 1px line
+  context.moveTo(40, 10.5);
+  context.lineTo(50, 10.5);
+  context.stroke();
+
+  context.fillRect(55, 10, 1, 1);
+
+  context.beginPath();
+  context.arc(10, 10, 5, 0, 2 * Math.PI);
+  context.fill();
+
+  fillCircle(context, 10, 30, 1, "#ffffff");
+  fillCircle(context, 16, 30, 2, "#ffffff");
+  fillCircle(context, 24, 30, 3, "#ffffff");
+  fillCircle(context, 36, 30, 4, "#ffffff");
+  fillCircle(context, 48, 30, 5, "#ffffff");
+  fillCircle(context, 66, 30, 6, "#ffffff");
+  fillCircle(context, 84, 30, 7, "#ffffff");
+  fillCircle(context, 104, 30, 8, "#ffffff");
 
   const buffer = canvas.toBuffer("raw");
 
---
+++ src/sprite.d.ts
@@ -0,0 +1,9 @@
+import { Image } from "canvas";
+
+export interface Sprite {
+  image: Image;
+  x: number;
+  y: number;
+  flash: boolean;
+  flashDuration: number;
+}
